84c3ce241bdbb6387be0b38a5cc843b43b119f7c
ri_yarm <thogusus@gmail.com>
2024-02-29 16:00:27 +0500

    delete: console.log

--- src/lib/functions/getTitle.ts
+++ src/lib/functions/getTitle.ts
@@ -1,7 +1,6 @@
 import { formatDate } from "lib/functions/formatDate.ts";
 
 export const getTitle = (date: string, contributions: number) => {
-  console.log(date);
   return contributions === 0
     ? `No contributions\n${formatDate(date)}`
     : `${contributions} contributions\n${formatDate(date)}`;
